b3d6bc3865899c1117a605312ee9b6b2c9f5a91f
koya-namba <namba.koya@arakawa-lab.com>
2022-10-06 10:38:57 +0900

    setting & create migration

---
+++ database/factories/MusicPostFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\MusicPost;
+use Faker\Generator as Faker;
+
+$factory->define(MusicPost::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
