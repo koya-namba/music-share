2317eea07c3b947968684ee8dc4256428e9f733c
koya-namba <namba.koya@arakawa-lab.com>
2022-10-07 21:23:42 +0900

    create music app

--- app/Http/Controllers/MusicPostController.php
+++ app/Http/Controllers/MusicPostController.php
@@ -4,19 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\MusicPost;
+use Storage;
 
 class MusicPostController extends Controller
 {
     public function index(MusicPost $music_post)
     {
         return view('posts/index')
-        ->with(['music_posts' => $music_post->getPaginateByLimit()]);
-    }
-    
-    public function show(MusicPost $music_post)
-    {
-        return view('posts/show')
-        ->with(['music_post' => $music_post]);
+        ->with(['music_posts' => $music_post->get()]);
     }
     
     public function create()
@@ -26,10 +21,17 @@ class MusicPostController extends Controller
     
     public function store(Request $request, MusicPost $music_post)
     {
-        $input = $request['music_post'];
-        dd($request->file('audio'));
-        $audio = $input->file('audio');
+        $music_post->title = $request['title'];
         
-        // $path = Storage::disk('s3')->putFile('')
+        //s3アップロード開始
+        $audio = $request->file('audio');
+        // バケットの`myprefix`フォルダへアップロード
+        $path = Storage::disk('s3')->putFile('myprefix', $audio, 'public');
+        // アップロードしたaudioのフルパスを取得
+        $music_post->audio_path = Storage::disk('s3')->url($path);
+
+        $music_post->save();
+        
+        return redirect('/');
     }
 }
--- database/migrations/2022_10_06_013454_create_music_posts_table.php
+++ database/migrations/2022_10_06_013454_create_music_posts_table.php
@@ -16,8 +16,7 @@ class CreateMusicPostsTable extends Migration
         Schema::create('music_posts', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('title', 100);
-            $table->string('detail', 255)->nullable();
-            $table->string('image_path', 255)->nullable();
+            $table->string('audio_path', 255)->nullable();
             $table->timestamps();
         });
     }
